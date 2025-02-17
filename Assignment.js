12ee0026e041f1de797bff3efe3d59625db68a6f
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2025-02-17 23:09:46 +0600

    assignment

---
+++ Assignment.js
@@ -0,0 +1,11 @@
+
+// function calculateVAT( price ) {
+//     if (isNaN(price) || price < 0) {
+//         return "Invalid";
+//     }
+//     return price * (7.5/100);
+// }
+// // console.log(calculateVAT(200));
+// // console.log(calculateVAT(-50));  
+// // console.log(calculateVAT("abc")); 
+// // console.log(calculateVAT("foo"));

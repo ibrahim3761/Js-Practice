3d0557fd012523baecd7b01c729d836baad13142
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2025-02-18 23:21:04 +0600

    assignment

--- Assignment.js
+++ Assignment.js
@@ -9,3 +9,26 @@
 // // console.log(calculateVAT(-50));  
 // // console.log(calculateVAT("abc")); 
 // // console.log(calculateVAT("foo"));
+
+
+/*function signature/sample */
+function  validContact( contact ) {
+    if ( typeof contact!== "string") {
+        return "Invalid";
+    }
+
+    if (isNaN(contact) || contact.includes(" ")) {
+        return false;
+    }
+
+    if (contact.length !== 11) {
+        return false;
+    }
+    if (contact[0] !== "0" || contact[1] !== "1") {
+        return false;
+    }
+
+    return true;
+}
+
+console.log(validContact(true));

037e2cbca5f32e0887cf8ced22e54d898273563a
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2025-03-05 17:09:18 +0600

    spread op

---
+++ DOM/js/spread.js
@@ -0,0 +1,9 @@
+const friends = ["sokina", "jorina", "karima"]
+
+const newArray = ["hero alam",...friends]
+
+console.log(newArray);
+
+const numbers = [1,5,4,6,3,65,9,56,5,75,54,5];
+
+console.log(Math.max(...numbers));

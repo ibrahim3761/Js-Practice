f7499883efc7726c152c9be561c1f5b3745772f3
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-05-03 01:24:16 +0600

    ts-generic

---
+++ Typescript/src/generic.ts
@@ -0,0 +1,27 @@
+// dynmicaally generalize
+
+type GenericArraay<T> = Array<T>;
+
+// const friends : string[] = ["Alice", "Bob", "Charlie"];
+const friends: GenericArraay<string> = ["Alice", "Bob", "Charlie"];
+
+// const rollNumbers : number[] = [1, 2, 3];
+const rollNumbers: GenericArraay<number> = [1, 2, 3];
+
+// const isEligibleList : boolean[] = [true, false, true];
+const isEligibleList: GenericArraay<boolean> = [true, false, true];
+
+const userList: GenericArraay<{ name: string; age: number }> = [
+  { name: "John Doe", age: 30 },
+  { name: "Jane Smith", age: 25 },
+];
+
+const sqFunc = (input: number): number => input * input;
+
+type Coordinates<X, Y> = [X, Y];
+
+const coordinates1: Coordinates<number, number> = [20, 30];
+const coordinates2: Coordinates<string, string> = ["20", "30"];
+
+console.log(coordinates1, coordinates2);
+

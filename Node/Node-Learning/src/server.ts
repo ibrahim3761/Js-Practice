d4551245722754790ed46406226b977c3145ae14
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-05-11 11:45:23 +0600

    RouteHandling

--- Node/Node-Learning/src/server.ts
+++ Node/Node-Learning/src/server.ts
@@ -1,7 +1,23 @@
 import { createServer, IncomingMessage, Server } from "http";
 
 const server : Server = createServer((req : IncomingMessage,res)=>{
-    console.log(req);
+    // console.log(req.url); "/", "/user", "/products"
+    // console.log(req.method); "GET", "POST", "DELETE"
+    const url = req.url
+    const method = req.method
+    if(url === "/" && method === "GET"){
+        // console.log("This is Root route");
+        res.writeHead(200,{"content-type" : "application/json"})
+        res.end(JSON.stringify({message : "This is Root route"}));
+    }
+    else if(url?.startsWith('/products')){
+        res.writeHead(200,{"content-type" : "application/json"})
+        res.end(JSON.stringify({message : "This is Products route"}));
+    }
+    else{
+        res.writeHead(400,{"content-type" : "application/json"})
+        res.end(JSON.stringify({message : "Route not found"}));
+    }
 })
 
 server.listen(5000,()=>{

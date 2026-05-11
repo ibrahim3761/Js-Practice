dcc514b014354150b87da8b1dc55a89769029ec2
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-05-12 01:02:19 +0600

    post-method

--- Node/Node-Learning/src/controller/product.controller.ts
+++ Node/Node-Learning/src/controller/product.controller.ts
@@ -1,22 +1,25 @@
 import type { IncomingMessage, ServerResponse } from "http";
-import { readProduct } from "../service/product.service";
+import { readProduct, writeProduct } from "../service/product.service";
 import type { IProduct } from "../types/product.type";
+import { parseBody } from "../utility/parseBody";
 
-export const productController = (
+export const productController = async (
   req: IncomingMessage,
   res: ServerResponse,
 ) => {
+  //console.log("Req",req);
+        
   const url = req.url;
   const method = req.method;
 
   const urlParts = url?.split("/");
   console.log(urlParts);
-  const id = urlParts && urlParts[1] === 'products' ? Number(urlParts[2]) : null
-  console.log("This is prouct id :", id );
-  
+  const id =
+  urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
+  //console.log("This is prouct id :", id);
+
   //Get all products
   if (url === "/products" && method === "GET") {
-
     // const products = [
     //     {
     //         id : 1,
@@ -35,15 +38,39 @@ export const productController = (
     const products = readProduct();
 
     res.writeHead(200, { "content-type": "application/json" });
-    res.end(JSON.stringify({ message: "Products found successfully ", 
-    data: products }));
-  }
-  else if(method === "GET" && id !== null){
+    res.end(
+      JSON.stringify({
+        message: "Products found successfully ",
+        data: products,
+      }),
+    );
+  } 
+  else if (method === "GET" && id !== null) {
     const products = readProduct();
-    const product = products.find((p : IProduct )=>p.id === id)
-    console.log(product);
+    const product = products.find((p: IProduct) => p.id === id);
+    //console.log(product);
     res.writeHead(200, { "content-type": "application/json" });
-    res.end(JSON.stringify({ message: "Product found successfully ", 
-    data: product }));
+    res.end(
+      JSON.stringify({ message: "Product found successfully ", data: product }),
+    );
+  } 
+  else if (method === "POST" && url === "/products") {
+    const body = await parseBody(req);
+    console.log("Body",body);
+    const newProduct ={
+      id : Date.now(),
+      ...body
+    }
+    //console.log(newProduct);
+    const products = readProduct();
+    products.push(newProduct)
+    // console.log(products);
+    writeProduct(products)
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({ message: "Product created successfully ", 
+        data: products
+      }),
+    );
   }
 };
--- Node/Node-Learning/src/service/product.service.ts
+++ Node/Node-Learning/src/service/product.service.ts
@@ -6,6 +6,10 @@ const filePath = path.join(process.cwd(),"./src/database/db.json")
 export const readProduct =()=>{
     // console.log(filePath);
     const products = fs.readFileSync(filePath, "utf-8");
-    console.log(JSON.parse(products));
+    //console.log(JSON.parse(products));
     return JSON.parse(products);
+}
+
+export const writeProduct = (paylaod : any)=>{
+    fs.writeFileSync(filePath,JSON.stringify(paylaod))
 }

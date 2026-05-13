fbef447701e9003f2d3138056e82cd10249ab5b2
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-05-13 21:39:19 +0600

    environment_based_config

--- ExpressJS/Express/src/server.ts
+++ ExpressJS/Express/src/server.ts
@@ -4,17 +4,17 @@ import express, {
   type Response,
 } from "express";
 import { Pool } from "pg";
+import config from "./config";
 
 const app: Application = express();
-const port = 5000;
+const port = config.port;
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
 const pool = new Pool({
-  connectionString:
-    "postgresql://neondb_owner:npg_RBtIDd9piA6S@ep-square-king-aqi32w7s.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require",
+  connectionString: config.connection_string,
 });
 
 const initDB = async()=>{
